f16a91e73ef4c3da32add52862e18c519a495b0c
BlackF535 <blackferdinand535@gmail.com>
2024-12-02 00:18:37 +0100

    premier vrai pas de la vitrine

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-            console.log("Site SOC chargé avec succès !");
+document.addEventListener("DOMContentLoaded", () => {
+            const animatedElements = document.querySelectorAll('.animate__animated');
+            
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('animate__fadeIn');
+                    }
+                });
+            }, { threshold: 0.1 });
+        
+            animatedElements.forEach(element => {
+                observer.observe(element);
+            });
         });
         
